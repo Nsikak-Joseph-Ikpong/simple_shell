69967ef2f8ae3f42557d29a32d2aa46a2c7ff8df
enedaveeed <enzydegreat17@gmail.com>
2023-11-14 05:58:48 -0800

    Simple Shell

--- shell_lists1.c
+++ shell_lists1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
+ * list_len - determines length of linked list, Calculates the size of a linked list.
  * @h: pointer to first node
  *
  * Return: size of list
@@ -19,7 +19,7 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
+ * list_to_strings - returns an array of strings of the list->str, Produces a string array from the conversion of the list to a string.
  * @head: pointer to first node
  *
  * Return: array of strings
@@ -56,7 +56,7 @@ char **list_to_strings(list_t *head)
 
 
 /**
- * print_list - prints all elements of a list_t linked list
+ * print_list - prints all elements of a list_t linked list, Outputs every element in a linked list of type list_t.
  * @h: pointer to first node
  *
  * Return: size of list
@@ -79,7 +79,7 @@ size_t print_list(const list_t *h)
 }
 
 /**
- * node_starts_with - returns node whose string starts with prefix
+ * node_starts_with - returns node whose string starts with prefix, Provides the node whose string commences with the specified prefix.
  * @node: pointer to list head
  * @prefix: string to match
  * @c: the next character after prefix to match
@@ -101,7 +101,7 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 }
 
 /**
- * get_node_index - gets the index of a node
+ * get_node_index - gets the index of a node, Obtains the node's index.
  * @head: pointer to list head
  * @node: pointer to the node
  *
